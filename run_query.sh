8a86b6aa011958b5c90d81c2c44fd892cce053f1
krithiksundar <krithiksundar@gmail.com>
2025-11-15 13:58:38 +0530

    Update run_query.sh

--- run_query.sh
+++ run_query.sh
@@ -13,10 +13,19 @@ echo "192.168.0.107 sdb sdb.localdomain" >> /etc/hosts
 echo "Connecting to Oracle and executing query..."
 > output.txt
 
-echo "Attempting SQLPlus connection (timeout: 60 seconds)..."
 
-# ---- FIX: wrap heredoc inside bash -c ----
-timeout 240 sqlplus "$DB_USER/$DB_PASS@$DB_TNS"
+echo "Testing Oracle DB connection..."
+echo "--------------------------------"
+
+timeout 120 sqlplus -S "$DB_USER/$DB_PASS@$DB_TNS" <<EOF
+SET HEADING OFF
+SET FEEDBACK OFF
+SELECT 'Connection Successful!' FROM dual;
+EXIT;
+EOF
+
+echo "--------------------------------"
+echo "Done"
 
 
 
